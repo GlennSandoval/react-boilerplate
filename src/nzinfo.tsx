2374e4f35b6b31adcdb7b0b51ac302d3a336a7c3
Glenn Sandoval <glenn.sandoval@gmail.com>
2020-09-06 10:59:38 +1200

    Fix Babel polyfill Add Router Add Example page for axios

---
+++ src/nzinfo.tsx
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+export default function NZInfo() {
+  const [payload, setPayload] = useState<any>({});
+
+  useEffect(() => {
+    async function getNzInfo() {
+      let payload = await axios.get('https://restcountries.eu/rest/v2/alpha/nz');
+      setPayload(payload);
+    }
+
+    getNzInfo();
+  });
+
+  return (
+    <div>
+      <pre>
+        {JSON.stringify(payload, null, 2)}
+      </pre>
+    </div>
+  )
+
+};
