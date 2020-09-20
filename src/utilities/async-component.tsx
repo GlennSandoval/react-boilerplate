7dc144504e6861e61e91b272c3f1663fe57cc11b
Glenn Sandoval <glenn.sandoval@gmail.com>
2020-09-20 12:33:54 +1200

    Clean up async component

--- src/utilities/async-component.tsx
+++ src/utilities/async-component.tsx
@@ -1,104 +1,52 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
+
+function wait(delay: number) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
 
 /**
  * Retry an asynchronous function
  * @param fn - async function
- * @param retriesLeft - number of times to retry
+ * @param attempts - number of times to retry
  * @param interval - interval in ms to wait between retries
- * @returns {Promise<any>}
  */
-function retry(fn: Function, retriesLeft = 3, interval = 1500): Promise<any> {
-  return new Promise((resolve, reject) => {
-    fn()
-      .then(resolve)
-      .catch((error: any) => {
-        setTimeout(() => {
-          if (retriesLeft === 1) {
-            // reject('maximum retries exceeded');
-            reject(error);
-            return;
-          }
-
-          // Passing on "reject" is the important part
-          retry(fn, retriesLeft - 1, interval).then(resolve, reject);
-        }, interval);
-      });
-  });
+async function retry(fn: Function, attempts = 3, interval = 1500) {
+  let result = null;
+  let tries = 0;
+  while (tries < attempts) {
+    try {
+      result = await fn();
+    } catch {
+      await wait(interval);
+    }
+    tries++;
+  }
+  return result;
 }
 
 /**
  * Create a wrapper around a component to load it asynchronously.
  * @example
  * const Component = asyncComponent(() => import('./views/pages/Component'));
- *
- * @param {function} importComponent
- * @param {Element} Placeholder - UI to show while component is loading
- * @returns {AsyncComponent}
  */
 export default function asyncComponent(importComponent: Function, Placeholder = (<div />)) {
-  /**
-   * Wrapper component that handles the asynchronous loading of components.
-   */
-  class AsyncComponent<P = {}> extends Component<any> {
+  return function (props: any | null) {
+    const [Inner, setInner] = useState<typeof React.Component | null>(null);
 
-    unMounted: boolean;
-
-    state: {
-      component: typeof React.Component | null,
-      error: any
-    }
-
-    constructor(props: Readonly<P>) {
-      super(props);
-
-      this.unMounted = false;
-
-      this.state = {
-        // This will reference the component once loaded
-        component: null,
-        error: null,
-      };
-    }
-
-    async componentDidMount() {
-      const self = this;
-      const result: { default: Component<any, any, any> } = await retry(importComponent)
-        .catch((error) => {
-          if (!self.unMounted) {
-            self.setState({error});
-          }
-        });
-
-      if (self.unMounted || !result?.default) {
-        return;
+    useEffect(() => {
+      async function load() {
+        let payload = await retry(importComponent);
+        setInner(payload);
       }
 
-      // Once the component loads, calling setState will trigger a render of the now loaded component.
-      self.setState({
-        component: result.default,
-        error: null,
-      });
+      load().catch(e => setInner(e));
+    });
+
+    if (Inner != null) {
+      return (<Inner {...props} />);
     }
 
-    componentWillUnmount() {
-      this.unMounted = true;
-    }
-
-    render() {
-      const {component: WrappedComponent, error} = this.state;
-
-      if (error) {
-        throw error;
-      }
-
-      // If the component is loaded, render it.
-      if (WrappedComponent != null) {
-        return (<WrappedComponent {...this.props} />);
-      }
-
-      return Placeholder;
-    }
+    return Placeholder;
   }
 
-  return AsyncComponent;
 }
